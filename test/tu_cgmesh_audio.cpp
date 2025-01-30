1e0b9b775a31c972853de37c3c09a7bcdef30c58
exopyl <exopyl74@mail.ru>
2025-01-30 19:23:03 +0100

    test audio

---
+++ test/tu_cgmesh_audio.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+#include "../src/cgmesh/cgmesh.h"
+
+
+TEST(TEST_cgmesh_audio, audio_2_image)
+{
+    // context
+    char* input = (char*)"./test/data/M1F1-Alaw-AFsp.wav";
+    Audio* audio = new Audio();
+    audio->load(input);
+    ASSERT_EQ(audio->get_length(), 145969);
+    audio->dump();
+
+    // action
+    Img* img = audio_2_image(audio);
+
+    // expectations
+    int ret = img->save("audio.bmp");
+    EXPECT_EQ(ret, 0);
+}
