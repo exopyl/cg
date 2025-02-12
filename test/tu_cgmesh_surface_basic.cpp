ffa5bc5d3fe1aa80622200c8c0acb70cd4bd4581
exopyl <exopyl74@mail.ru>
2025-02-12 18:29:50 +0100

    test for surface basic

---
+++ test/tu_cgmesh_surface_basic.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include "../src/cgmesh/cgmesh.h"
+
+
+TEST(TEST_cgmesh_surface_basic, teapot)
+{
+    // action
+    Mesh* mesh = CreateTeapot();
+
+    // expectations
+    EXPECT_EQ(mesh->GetNVertices(), 1767);
+    EXPECT_EQ(mesh->GetNFaces(), 2256);
+}
+
+TEST(TEST_cgmesh_surface_basic, klein_bottle)
+{
+    // action
+    Mesh* mesh = CreateKleinBottle(10, 10);
+
+    // expectations
+    EXPECT_EQ(mesh->GetNVertices(), 121);
+    EXPECT_EQ(mesh->GetNFaces(), 200);
+}
