64b4a4eebe9bf2be9e58cd61cbc685bb64020da2
exopyl <exopyl74@mail.ru>
2026-02-25 22:06:11 +0100

    fix

--- test/tu_cgmath_curve_kappatau.cpp
+++ test/tu_cgmath_curve_kappatau.cpp
@@ -1,11 +1,11 @@
 #include <gtest/gtest.h>
-
+#include <cmath>
 #include "../src/cgmath/curve_kappatau.h"
 
 static float constant_kappa(float s) { (void)s; return 1.f; }
 static float zero_tau(float s) { (void)s; return 0.f; }
 static float variable_kappa(float s) { return 8.f * s; }
-static float sinusoidal_tau(float s) { return sinf(s) + 3.f * sinf(2.f * s); }
+static float sinusoidal_tau(float s) { return std::sin(s) + 3.f * std::sin(2.f * s); }
 
 TEST(TEST_cgmath_curve_kappatau, Constructor)
 {
